763f933110168b788e8e4ec3993aa2df1ca7c3b7
Bizzozzéro Nicolas <nicolasbizzozzero@gmail.com>
2016-12-26 10:40:16 +0100

    Add files via upload

--- download_youtube_musics_from_a_file.py
+++ download_youtube_musics_from_a_file.py
@@ -2,16 +2,41 @@ from sys import argv
 from os import system as run_command
 
 
-def create_a_correct_command(youtube_url):
-    return "youtube-dl --extract-audio --audio-format m4a --audio-quality 0 --embed-thumbnail " + youtube_url + " -o C:/Users/Nicolas/Downloads///%(title)s.%(ext)s --prefer-ffmpeg --ffmpeg-location \"C:/Program Files (x86)/Youtube DL/ffmpeg-20160922-7d17d31-win64-static/bin\""
+def create_a_correct_command(youtube_url: str) -> str:
+    return (r"youtube-dl "
+            #            "--extract-audio "
+            #            "--audio-format m4a "
+            #            "--audio-quality 0 "
+            "--embed-thumbnail "
+            "-o C:/Users/Nicolas/Downloads///%(title)s.%(ext)s "
+            #            "--prefer-ffmpeg "
+            #            "--ffmpeg-location C:/Program Files (x86)/Youtube DL/ffmpeg-20160922-7d17d31-win64-static/bin"
+            "--limit-rate 20K "
+            "--ignore-errors "
+            + youtube_url
+            )
+
 
 if __name__ == '__main__':
     if (len(argv) != 2):
-        print(
-            "You need to pass at least one argument.\nUsage : download_youtube_music_from_a_file.py [file]")
+        print(("You need to pass at least one argument.\n"
+               "Usage : download_youtube_music_from_a_file.py [file]"))
     else:
-        with open(argv[1], 'r') as file:
+        with open(argv[1]) as file:
             for youtube_url in file:
                 command = create_a_correct_command(youtube_url)
                 run_command(command)
                 # Delete music from file IF THE DL IS A SUCCESS
+
+# Commands to copy paste :
+# "youtube-dl --extract-audio --audio-format m4a --audio-quality 0 --embed-thumbnail -o C:/Users/Nicolas/Downloads///%(title)s.%(ext)s --prefer-ffmpeg --ffmpeg-location C:/Program Files (x86)/Youtube DL/ffmpeg-20160922-7d17d31-win64-static/bin"
+# Limit rate : --limit-rate 50K
+# Playlist range : --playlist-items 1-16,18-62,64-13
+# Ignorer les erreurs : --ignore-errors
+
+# Exemples :
+# Telecharger à 30 Kio/s toute une playlist au meilleur format disponible en numérotant le numéro de la playlist dans le titre et avec des indexs bien précis, et ne s'arrete pas si il rencontre une vidéo qui n'existe plus
+# youtube-dl --embed-thumbnail -o
+# "C:/Users/Nicolas/Downloads///%(playlist_index)s - %(title)s.%(ext)s"
+# --limit-rate 30K --ignore-errors
+# https://www.youtube.com/playlist?list=PL7XlqX4npddfrdpMCxBnNZXg2GFll7t5y
