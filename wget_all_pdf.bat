64538ee89ebe3cbf46ab4822322a07173bc65675
Bizzozzéro Nicolas <nicolasbizzozzero@gmail.com>
2017-12-03 21:56:10 +0100

    Add files via upload

---
+++ wget_all_pdf.bat
@@ -0,0 +1,3 @@
+wget --no-parent --content-disposition -e robots=off -Apdf -rH -o "log.txt" -t 2 -c --no-check-certificate http://diplome.di.ens.fr/
+
+REM wget --no-parent --content-disposition -e robots=off -Apdf -d -rH -nd --no-check-certificate -Dmedia.blueapron.com -i recipes.txt
