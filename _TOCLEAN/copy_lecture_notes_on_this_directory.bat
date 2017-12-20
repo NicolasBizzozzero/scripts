1c26e060b69ae07564f497956f01f350421bb1a2
NicolasBi <nicolasbizzozzero@gmail.com>
2017-12-20 01:23:40 +0100

    Finished asciify.py

--- _TOCLEAN/copy_lecture_notes_on_this_directory.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-del *notes.txt
-xcopy "C:\Users\Nicolas\Desktop\Cours actuellement etudies\[3I003] - Algorithmique\3I003-algorithmique lecture notes.txt" .
-xcopy "C:\Users\Nicolas\Desktop\Cours actuellement etudies\[3I004] - Architecture\3I004-architecture lecture notes.txt" .
-xcopy "C:\Users\Nicolas\Desktop\Cours actuellement etudies\[3I006] - Logique\3I006-logique lecture notes.txt" .
-xcopy "C:\Users\Nicolas\Desktop\Cours actuellement etudies\[3I014] - Reseaux\3I014-reseaux lecture notes.txt" .
-xcopy "C:\Users\Nicolas\Desktop\Cours actuellement etudies\[3I015] - Administration systemes\3I015-sysadmin lecture notes.txt" .
