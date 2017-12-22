d808e03600e37798f5c6363202abe6fe44a25098
NicolasBi <nicolasbizzozzero@gmail.com>
2017-12-22 12:38:08 +0100

    Completed git_time.sh and git_history_cheat.py

---
+++ github_history_cheat.py
@@ -0,0 +1,69 @@
+""" Alter Github commit history.
+
+Dependencies :
+* https://github.com/NicolasBi/scripts/git_time.sh
+* click
+"""
+
+import subprocess
+import datetime
+import random
+import time
+
+import click
+
+
+_RANGE_NUMBER_COMMIS = (1, 4)
+
+_MONTHS = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+           "Oct", "Nov", "Dec")
+_FORMAT_TIME = "{month} {day} {hour}:{minute}:{second} {year} +0100"
+
+_GIT_TIME_SCRIPT = "./git_time.sh"
+
+_STR_WRONG_NUMBER_ARGUMENTS = "Error: Wrong number of arguments"
+_STR_WRONG_TIME_FORMAT = ("Error, the time \"{time}\"must respect the ffmpeg "
+                          "time format")
+
+_CODE_SUCCESS = 0
+_CODE_WRONG_NUMBER_ARGUMENT = 1
+_CODE_WRONG_TIME_FORMAT = 2
+
+
+@click.command()
+@click.parameter("git_time", type=click.File(exists=True, readable=True),
+                 default=".git_time.sh")
+@click.version_option("0.0.1")
+def main(git_time) -> None:
+    for month in _MONTHS[:-1]:
+        for day in range(1, 28):
+            today = datetime.datetime.now()
+            commit_time = _FORMAT_TIME.format(month=month,
+                                              day=day,
+                                              hour=today.hour,
+                                              minute=today.minute,
+                                              second=today.second,
+                                              year=today.year)
+
+            # Let's commit
+            for _ in range(random.randint(*_RANGE_NUMBER_COMMIS)):
+                _execute_command(_GIT_TIME_SCRIPT, [commit_time])
+
+    # Force push changes
+    _execute_command("git", ["push", "origin", "master", "--force"])
+    exit(_CODE_SUCCESS)
+
+
+def _execute_command(command: str, parameters: str, stdin=None, stdout=None,
+                     stderr=None) -> int:
+    """ Execute `command` in a subprocess and return the result code of the
+    command.
+    """
+    result = subprocess.run([command, *parameters], stdin=stdin, input=None,
+                            stdout=stdout, stderr=stderr, shell=False,
+                            timeout=None, check=False)
+    return result.returncode
+
+
+if __name__ == '__main__':
+    main()
